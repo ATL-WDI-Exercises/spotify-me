6df59234ab9e9911a0d34c0c96ed3ba7190ffff9
marcwright <marc.wright@gmail.com>
2016-01-31 14:01:18 -0500

    updated script.js

--- starter-code/public/javascripts/script.js
+++ starter-code/public/javascripts/script.js
@@ -1,12 +1,6 @@
 // API Docs at: 
 // https://developer.spotify.com/
 
-
-function searchByArtist(keyword) {
-  var url = 'https://api.spotify.com/v1/search?q=' + keyword + '&type=artist'
-}
-
-
-function searchByTrack(keyword) {
-  var url = 'https://api.spotify.com/v1/search?q=' + keyword + '&type=track'
-}
+//sample API endpoints
+// https://api.spotify.com/v1/search?q=' + keyword + '&type=artist
+// https://api.spotify.com/v1/search?q=' + keyword + '&type=track
